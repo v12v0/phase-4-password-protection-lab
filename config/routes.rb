350d5af1bb09d20adada9d21546a1ef8ee405959
Vi Vo <aaronvo118@gmail.com>
2022-04-09 11:35:40 -0500

    commit tree 358f9a033fd26c7528fd601bf2e49942586c426f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
